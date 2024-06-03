5bbc0b0869dedeb1e560948f0ff8abb86de4686d
gchai <g@bechai.com>
2024-06-03 17:59:16 -0400

    Chapter 5 assessment

---
+++ 5.1.6-JSDOM_Form_submission_Guild_Node_18_1/src/index.js
@@ -0,0 +1,76 @@
+// Note: Solution for this exercise is provided below. Study the solution
+// and try to implemented yourself.
+
+/*
+
+
+function validateForm(data) {
+  return data.get("searchTerm").trim() !== "";
+}
+
+
+const form = document.querySelector("#searchForm");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  let error = document.querySelector("#searchError");
+  if (!error) {
+    error = document.createElement("div");
+    error.classList.add("error");
+    error.id = "searchError";
+    error.innerHTML = "Please enter a search term";
+  } else {
+    form.removeChild(error);
+  }
+  const formData = new FormData(form);
+  if (!validateForm(formData)) {
+    form.appendChild(error);
+  } else {
+    const searchTerm = formData.get("searchTerm").trim().toLowerCase();
+    const titles = document.querySelectorAll("article > h2");
+    for (let title of titles) {
+      if (!title.innerHTML.toLowerCase().includes(searchTerm)) {
+        title.parentNode.classList.add("hidden");
+      } else {
+        title.parentNode.classList.remove("hidden");
+      }
+    }
+  }
+});
+
+*/
+
+
+function validateForm(data) {
+  return data.get("searchTerm").trim() !== "";
+}
+
+
+const form = document.querySelector("#searchForm");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  let error = document.querySelector("#searchError");
+  if (!error) {
+    error = document.createElement("div");
+    error.classList.add("error");
+    error.id = "searchError";
+    error.innerHTML = "Please enter a search term";
+  } else {
+    form.removeChild(error);
+  }
+  const formData = new FormData(form);
+  if (!validateForm(formData)) {
+    form.appendChild(error);
+  } else {
+    const searchTerm = formData.get("searchTerm").trim().toLowerCase();
+    const titles = document.querySelectorAll("article > h2");
+    for (let title of titles) {
+      if (!title.innerHTML.toLowerCase().includes(searchTerm)) {
+        title.parentNode.classList.add("hidden");
+      } else {
+        title.parentNode.classList.remove("hidden");
+      }
+    }
+  }
+});
